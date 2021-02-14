a875d4d703f49c82e2adc08f56f497ed467e4d3a
Drip.Flux <fv43k2e9x7@liamekaens.com>
2021-02-14 09:52:36 -0500

    Initial commit. Added project material (qrPNG.py: add; qrSVG.py: add).

---
+++ qrPNG.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Description: Generate QR code of argv[1] into SVG.
+Arguments:
+	argv[1] : (Required) String to generate QR code of.
+	argv[2] : (Required) Path/basename of SVG file to generate.
+Return:
+	0  : (normal).
+	1+ : ERROR.
+"""
+
+import sys  # System Module: argv[], exit()
+import pyqrcode  # QR Code Module: create(), qrObj.svg()
+
+def main(
+		cmdArgv=None
+	) :
+	"""
+	Description: Main control flow of program.
+	Arguments:
+		cmdArgv : Command line arguments, expect same format as sys.argv
+	Return:
+		None  : (normal)
+		1+ : ERROR, via sys.exit()
+	"""
+	# Setup environment
+	if cmdArgv is None :
+		argv = sys.argv[:]
+	else :
+		argv = cmdArgv
+	# Validate environment
+	if len(argv) != 3 :
+		sys.exit(1)
+	# Core Functionality
+	qrObj = pyqrcode.create(argv[1])
+	qrObj.png(argv[2]+".png", scale=4)
+
+if __name__ == "__main__" :
+	main()
